0c73c8bd4da85f1eca2dc8d8178fad240a0e892f
dumplinger <milena.d.industrial@gmail.com>
2021-08-14 17:05:48 -0300

    agregue arrays para la entrega 6

--- js/main.js
+++ js/main.js
@@ -14,11 +14,38 @@ const cordon = new restaurante('Torrente', 'Ratatouille', 'Meryl', 'Ramona')
 const malvin = new restaurante('Cantón Chino', 'La Pasiva', 'Ramen Noodles', 'Ramona')
 
 
+//ARRAY
 
-console.table(vegetariano)
-console.table(carnes)
-console.table(china)
+const restaurantes = [ 
+    { id: 1,  nombre: "Guacamole", precio: 2, vegetariano: true, delivery: true, ubicacion:"Centro", tipoDeComida: "veggie"},
+    { id: 2,  nombre: "La Sibarita", precio: 3, vegetariano: true, delivery: true, ubicacion:"Cordon", tipoDeComida: "veggie"},
+    { id: 3,  nombre: "La Temeraria", precio: 2, vegetariano: true, delivery: true, ubicacion:"Parque Rodo", tipoDeComida: "veggie"},
+    { id: 4,  nombre: "Chipotle", precio: 2, vegetariano: true, delivery: true, ubicacion:"Malvin", tipoDeComida: "veggie"},
+    { id: 5,  nombre: "McDonalds", precio: 1, vegetariano: false, delivery: true, ubicacion:"Pocitos", tipoDeComida: "fast food"},
+    { id: 6,  nombre: "Burger King", precio: 1, vegetariano: false, delivery: true, ubicacion:"Cordon", tipoDeComida: "fast food"},
+    { id: 7,  nombre: "La Pasiva", precio: 2, vegetariano: false, delivery: true, ubicacion:"Centro", tipoDeComida: "fast food"},
+    { id: 8,  nombre: "Pearsons", precio: 3, vegetariano: true, delivery: true, ubicacion:"Carrasco", tipoDeComida: "barbacoa"},
+    { id: 9,  nombre: "Cantón Chino", precio: 2, vegetariano: false, delivery: true, ubicacion:"Pocitos", tipoDeComida: "china"},
+    { id: 10,  nombre: "Sui Yuan", precio: 1, vegetariano: true, delivery: true, ubicacion:"Centro", tipoDeComida: "china"},
+    { id: 11,  nombre: "Arrolladitos", precio: 1, vegetariano: true, delivery: true, ubicacion:"Palermo", tipoDeComida: "china"},
+    { id: 12,  nombre: "Ramen Noodles", precio: 1, vegetariano: true, delivery: true, ubicacion:"Parque Rodo", tipoDeComida: "china"},
+    { id: 13,  nombre: "Berretin", precio: 3, vegetariano: false, delivery: true, ubicacion:"Pocitos", tipoDeComida: "barbacoa"},
+    { id: 14,  nombre: "Ratatouille", precio: 3, vegetariano: false, delivery: true, ubicacion:"Tres Cruces", tipoDeComida: "gourmet"},
+]
 
+
+//FUNCION FILTER
+const restaurantesOpcionesVegetarianos = restaurantes.filter(restaurantes => restaurantes.vegetariano === true); 
+console.log(restaurantesOpcionesVegetarianos);
+
+const restaurantesUbicacion = restaurantes.filter(restaurantes => restaurantes.ubicacion === "Centro"); 
+console.log(restaurantesUbicacion);
+
+const restaurantesBaratos = restaurantes.filter(restaurantes => restaurantes.precio < 2); 
+console.log(restaurantesUbicacion);
+
+
+//PROMPT
 
 let entrada = prompt("¿Querés pedir delivery o salir a comer?");
    switch (entrada) {
